00f4edf34ca0b9b13444496c922aad6f23b1a6c6
FedericoBilotti <fbilotti@uade.edu.ar>
2026-03-23 20:42:45 -0300

    feat: se añadieron las transacciones a las matrices

--- TPO1_Programacion.py
+++ TPO1_Programacion.py
@@ -1,5 +1,5 @@
 accounts = [[1, "Mercado Pago", 1000000], [2, "Galicia", 4395000], [3, "Efectivo", 500000]]
-transactions = []
+transactions = [[1, "2-3-2026", "20:20", 5000000, "Sueldo"], [2, "11-3-2026", "13:21", -5000, "Chocolate del Kiosco"], [3, "12-3-2026", "14:55", -200000, "Expensas"], [4, "13-3-2026", "00:02", -400000, "Matricula"]]
 categories = [[1,"Sueldo"], [2, "Alimento"], [3, "Gastos mensuales"],[4,"Educación"]]
 budgets = [[1,3,20000],[2,2,150000],[3,1,120000],[4,4,300000]]
  
