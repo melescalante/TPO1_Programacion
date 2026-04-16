56fb530a52cb07e3264ab581d13b0c9edb573eb7
Melina Escalante <melescalante@uade.edu.ar>
2026-04-15 21:03:58 -0300

    feat: comentarios en funcion de permisos

--- permissions.py
+++ permissions.py
@@ -6,6 +6,11 @@ ROLE_LEVELS = {
 }
 
 def has_permission(user, required_level):
+    """
+    user: diccionario con información del usuario logueado o None
+    required_level: nivel de permiso requerido (1=user, 2=admin)
+    Retorna: True si el usuario tiene el nivel de permiso requerido, False en caso contrario
+    """
     if not user:
         print('Debes logearte para usar el sistema')
         return
